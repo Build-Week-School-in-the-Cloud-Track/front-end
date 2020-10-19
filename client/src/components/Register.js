434ae013336d0c0597cffed242e087089ecf382c
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-19 14:19:29 -0500

    added register component to app route for testing

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 import * as yup from 'yup';
 
 const initialFormValues = {
