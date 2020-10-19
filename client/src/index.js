df229a1896964f4ef52f446c0f9f95bd711371f0
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-19 12:38:21 -0500

    installed react router and wrapped app in router

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
