6d9c109a724f42bf0e88f4d7b4a4941a12d04cb6
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-10-20 11:18:23 -0400

    taskform

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Register from "./components/Register";
+import Taskform from './components/Taskform';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/register" component={Register} />
+        <Route path="/register" component={Register}>
+         <Register />
+        </Route>
+        <Route path="/taskform" component={Taskform}>
+          <Taskform />
+        </Route>
       </Switch>
     </div>
   );
