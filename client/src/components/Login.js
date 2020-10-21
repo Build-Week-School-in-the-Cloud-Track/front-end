0c2042f66c6266d0816e9834599087b240c92634
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-20 23:11:34 -0500

    cleaned up components by moving things to different files

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,147 +2,35 @@ import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import { Link, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-import styled from "styled-components";
-import img from "../img/clouds1.jpg";
+import { StyledLogin } from "../StyledComponents/StyledLogin";
 import { userLogin } from "../actions";
+import { loginSchema } from "./validation/loginSchema";
 
-export const StyledLogin = styled.div`
-  form {
-    display: flex;
-    flex-direction: column;
-    width: 90%;
-    max-width: 300px;
-    margin: 0 auto;
-  }
+const defaultformValues = {
+  email: "",
+  password: "",
+};
 
-  .container {
-    height: 100vh;
-    background-image: url(${img});
-  }
-
-  h2 {
-    margin: 6% 0 4% 0;
-  }
-
-  label {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin: 10px 0;
-    font-size: 1.4rem;
-  }
-
-  input,
-  textarea {
-    width: 100%;
-    margin: 5px 0 0;
-    display: block;
-    width: 100%;
-    border: 2px solid rgba(0, 0, 0, 0.2);
-    border-radius: 5px;
-    padding: 10px;
-    transition: all 0.3s;
-    font-size: 1.4rem;
-    letter-spacing: 0.5px;
-    background-color: white;
-  }
-  select {
-    height: 40px;
-    width: 325px;
-  }
-
-  select:focus {
-    outline: none;
-    border-color: #0099ff;
-  }
-
-  input:focus {
-    outline: none;
-    border-color: #0099ff;
-  }
-
-  input:placeholder {
-    color: #a8b2b2;
-  }
-
-  button {
-    width: 150px;
-    display: inline-block;
-    padding: 8px 11px;
-    font-size: 1.2rem;
-    text-transform: uppercase;
-    border: 0;
-    border-radius: 5px;
-    letter-spacing: 2px;
-    outline: none;
-    background-color: #0099ff;
-    background-image: linear-gradient(to right, #0099ff, #ccf5ff);
-    color: #fff;
-    cursor: pointer;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
-  }
-
-  button:hover {
-    background-image: none;
-    background-color: #ccf5ff;
-    box-shadow: 0 5px 12px rgba(0, 0, 0, 0.3);
-  }
-
-  button:disabled {
-    background-image: none;
-    background-color: white;
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    color: rgba(0, 0, 0, 0.1);
-    cursor: not-allowed;
-  }
-
-  .error {
-    font-size: 1.2rem;
-    color: red;
-  }
-
-  .terms {
-    display: inline-block;
-    align-self: self-start;
-  }
-
-  .terms input {
-    width: 15px;
-    display: inline-block;
-    margin-right: 5px;
-  }
-`;
-
-const formSchema = yup.object().shape({
-  email: yup.string().required("please provide email"),
-  password: yup
-    .string()
-    .required("please enter a password")
-    .min(5, "password is too short"),
-});
+const defaultErrorValues = {
+  email: "",
+  password: "",
+  requestError: "",
+};
 
 function Login(props) {
-  const defaultformValues = {
-    email: "",
-    password: "",
-  };
-
   const [formValues, setFormValues] = useState(defaultformValues);
-  const [errorState, setErrorState] = useState({
-    ...defaultformValues,
-    requestError: "",
-  });
+  const [errorState, setErrorState] = useState(defaultErrorValues);
   const [disabled, setDisabled] = useState(true);
 
   const history = useHistory();
 
   useEffect(() => {
-    formSchema.isValid(formValues).then(valid => setDisabled(!valid));
-  }, [formValues, formSchema]);
+    loginSchema.isValid(formValues).then(valid => setDisabled(!valid));
+  }, [formValues, loginSchema]);
 
   const validate = e => {
     yup
-      .reach(formSchema, e.target.name)
+      .reach(loginSchema, e.target.name)
       .validate(e.target.value)
       .then(valid => {
         setErrorState({
@@ -166,7 +54,6 @@ function Login(props) {
 
   const formSubmit = e => {
     e.preventDefault();
-
     const user = {
       email: formValues.email.trim(),
       password: formValues.password.trim(),
@@ -196,7 +83,7 @@ function Login(props) {
           <label>
             Email
             <input
-              type="text"
+              type="email"
               name="email"
               id="email"
               value={formValues.email}
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,111 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { Link, useHistory } from "react-router-dom";
+import { registerSchema } from "./validation/registerSchema";
+import { StyledRegister } from "../StyledComponents/StyledRegister";
 import * as yup from "yup";
-import styled from "styled-components";
-import img from "../img/clouds1.jpg";
-
-export const StyledRegister = styled.div`
-  form {
-    display: flex;
-    flex-direction: column;
-    width: 90%;
-    max-width: 300px;
-    margin: 0 auto;
-  }
-  .container {
-    height: 100vh;
-    background-image: url(${img});
-  }
-
-  label {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin: 10px 0;
-    font-size: 1.4rem;
-  }
-
-  input,
-  textarea {
-    width: 100%;
-    margin: 5px 0 0;
-    display: block;
-    width: 100%;
-    border: 2px solid rgba(0, 0, 0, 0.2);
-    border-radius: 5px;
-    padding: 10px;
-    transition: all 0.3s;
-    font-size: 1.4rem;
-    letter-spacing: 0.5px;
-    background-color: white;
-  }
-  select {
-    height: 40px;
-    width: 325px;
-  }
-
-  select:focus {
-    outline: none;
-    border-color: #0099ff;
-  }
-
-  input:focus {
-    outline: none;
-    border-color: #0099ff;
-  }
-
-  input:placeholder {
-    color: #a8b2b2;
-  }
-
-  button {
-    width: 150px;
-    display: inline-block;
-    padding: 8px 11px;
-    font-size: 1.2rem;
-    text-transform: uppercase;
-    border: 0;
-    border-radius: 5px;
-    letter-spacing: 2px;
-    outline: none;
-    background-color: #0099ff;
-    background-image: linear-gradient(to right, #0099ff, #ccf5ff);
-    color: #fff;
-    cursor: pointer;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
-  }
-
-  button:hover {
-    background-image: none;
-    background-color: #ccf5ff;
-    box-shadow: 0 5px 12px rgba(0, 0, 0, 0.3);
-  }
-
-  button:disabled {
-    background-image: none;
-    background-color: white;
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    color: rgba(0, 0, 0, 0.1);
-    cursor: not-allowed;
-  }
-
-  .error {
-    font-size: 1.2rem;
-    color: red;
-  }
-
-  .terms {
-    display: inline-block;
-    align-self: self-start;
-  }
-
-  .terms input {
-    width: 15px;
-    display: inline-block;
-    margin-right: 5px;
-  }
-`;
 
 const initialFormValues = {
   email: "",
@@ -130,29 +28,10 @@ function Register() {
   const [disabled, setDisabled] = useState(initialDisabled);
   const history = useHistory();
 
-  //FormSchema Below
-  const registerFormSchema = yup.object().shape({
-    email: yup
-      .string()
-      .email("Must be a valid email")
-      .required("Email is required to register"),
-    password: yup
-      .string()
-      .required("Password is required")
-      .min(5, "Must be at least five characters"),
-    name: yup
-      .string()
-      .required("Name is Required")
-      .min(3, "Must be at least three characters"),
-    role: yup
-      .string()
-      .oneOf(["1", "2", "3"], "Please select student, admin, or volunteer")
-      .required("Selection required"),
-  }); //End of FormSchema
   // inputChange function below
   const inputChange = (name, value) => {
     yup
-      .reach(registerFormSchema, name)
+      .reach(registerSchema, name)
       .validate(value)
       .then(() => {
         setFormErrors({
@@ -199,7 +78,7 @@ function Register() {
 
   //UseEffect for schema validations when formValues change
   useEffect(() => {
-    registerFormSchema.isValid(formValues).then(valid => {
+    registerSchema.isValid(formValues).then(valid => {
       setDisabled(!valid);
     });
   }, [formValues]);
